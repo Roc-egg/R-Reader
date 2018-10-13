4c707320d1a5b8fe72ea077e475e381ed93dc500
hp <1033752447@qq.com>
2018-10-14 03:44:06 +0800

    上传xrecycleview

--- app/src/main/java/hp/redreader/com/app/base/BaseHeadeActivity.kt
+++ app/src/main/java/hp/redreader/com/app/base/BaseHeadeActivity.kt
@@ -4,10 +4,10 @@ package hp.redreader.com.app.base
  * 类名：    R-Reader
  * 类描述：
  * 创建人：  hp
- * 创建时间：2018/10/14/014 3:40
+ * 创建时间：2018/10/14/014 3:43
  * 修改人：  hp
- * 修改时间：2018/10/14/014 3:40
+ * 修改时间：2018/10/14/014 3:43
  * 修改备注：
  */
-class ABC {
+interface BaseHeadeActivity {
 }
