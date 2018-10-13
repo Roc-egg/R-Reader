d593702d57e69b74154cbb25bab7cd75a0a5ef35
hp <1033752447@qq.com>
2018-10-14 02:37:30 +0800

    test

--- app/src/main/java/hp/redreader/com/MainActivity.kt
+++ app/src/main/java/hp/redreader/com/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
